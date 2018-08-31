e29edfed0566597cc7c84479cc04fd92fd6283ae
Anwesh43 <anweshthecool0@gmail.com>
2018-09-01 02:33:08 +0530

    importing libraries and defining nodes

--- doublelinecircsweepview/src/main/java/com/anwesh/uiprojects/doublelinecircsweepview/DoubleLineCircSweepView.kt
+++ doublelinecircsweepview/src/main/java/com/anwesh/uiprojects/doublelinecircsweepview/DoubleLineCircSweepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.doublelinecircsweepview
 /**
  * Created by anweshmishra on 01/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5

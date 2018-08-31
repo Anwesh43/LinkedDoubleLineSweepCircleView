7298da58fd138e5829625d83cb4e7084da03c6ae
Anwesh43 <anweshthecool0@gmail.com>
2018-09-01 03:06:27 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddoublecircsweepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddoublecircsweepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkeddoublecircsweepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.doublelinecircsweepview.DoubleLineCircSweepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DoubleLineCircSweepView.create(this)
     }
 }

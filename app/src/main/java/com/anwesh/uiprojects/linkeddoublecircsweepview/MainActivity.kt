c1088e0a5d4f21b78391acee1ae8197b367fb2e8
Anwesh43 <anweshthecool0@gmail.com>
2018-09-01 12:33:02 +0530

    adding DLAnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddoublecircsweepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddoublecircsweepview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkeddoublecircsweepview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.doublelinecircsweepview.DoubleLineCircSweepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DoubleLineCircSweepView.create(this)
+        val view : DoubleLineCircSweepView = DoubleLineCircSweepView.create(this)
         fullScreen()
+        view.addAnimationListener({createToast("animation is ${it} is complete")}, {createToast("animation number ${it} is reset")})
+    }
+
+    private fun createToast(txt : String) {
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show()
     }
 }
 
